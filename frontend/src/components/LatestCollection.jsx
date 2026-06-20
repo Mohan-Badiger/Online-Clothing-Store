8566b64d44988f272405f192b781cfbcdaaa5145
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-06-20 22:22:12 +0530

    feat: implement frontend core features including collection filtering, search functionality, and product components

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import {ShopContext} from '../context/ShopContext'
 import Title from './Title';
 import ProductItem from './ProductItem';
@@ -10,7 +10,7 @@ const LatestCollection = () => {
 
     useEffect(()=>{
         setlatestProducts(products.slice(0,10));
-    },[])
+    },[products])
 
   return (
     <div className='my-10'>
@@ -32,4 +32,4 @@ const LatestCollection = () => {
   )
 }
 
-export default LatestCollection
+export default LatestCollection;
