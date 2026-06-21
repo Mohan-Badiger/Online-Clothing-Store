3b33f24ae9521fe724850a30a6c62cec3b419a0f
Mohan-Badiger <mohanbadiger250@gmail.com>
2026-06-21 22:23:43 +0530

    created Backend

---
+++ backend/models/productModel.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: [String], required: true },
+    category: { type: String, required: true },
+    subCategory: { type: String, required: true },
+    sizes: { type: [String], required: true },
+    date: { type: Number, required: true },
+    bestseller: { type: Boolean, default: false }
+});
+
+const productModel = mongoose.models.product || mongoose.model("product", productSchema);
+
+export default productModel;
